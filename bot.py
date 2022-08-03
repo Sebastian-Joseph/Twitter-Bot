60c78b7e7e4cdf4a3b34a5d9fb5b4832313fa246
Sebastian-Joseph <theclimax25@gmail.com>
2022-08-03 11:49:02 -0400

    initial commit

--- bot.py
+++ bot.py
@@ -1,16 +1,18 @@
-import tweepy, openai, mysql.connector, time
+import tweepy
+import openai
+import mysql.connector
+import time
+from time import sleep
 from important import api_key, api_secret, bearer_token, access_token, access_secret, client_id, client_secret, open_ai, sqlpass
+from config import *
 
 
-# twitter setup
+# twitter and ai setup
 
 auth = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
-
 openai.api_key = open_ai
-
-
 
 
 #db setup
@@ -21,11 +23,16 @@ mydb = mysql.connector.connect(
     password='22Sebastian$',
     database="twitterprompts"
 )
-
 mycursor = mydb.cursor()
 
+#set up for loop
 
-#loop that allows bot to post tweets
+search = '#nba'
+num_tweets = 10
+
+
+
+#loop that allows bot to post tweets from sql database
 
 while True:
     mycursor.execute("SELECT text FROM prompts ORDER BY RAND() LIMIT 1")
@@ -33,12 +40,45 @@ while True:
     response = openai.Completion.create(
         engine='text-davinci-001',
         prompt=myresult,
-        max_tokens=200,
+        max_tokens=50,
 
     )
     text = response.choices[0].text
     api.update_status(text)
-    time.sleep(3600)
+    time.sleep(30)
+
+    for tweet in api.search_tweets(q=QUERY):
+        try:
+            print('\nTweet by: @' + tweet.user.screen_name)
+
+
+            if LIKE:
+                if not tweet.favorited:
+                    tweet.favorite()
+                    print('Liked the tweet')
+
+
+            if FOLLOW:
+                if not tweet.user.following:
+                    tweet.user.follow()
+                    print('Followed the user')
+
+
+            if RETWEET:
+                if not tweet.retweeted:
+                    tweet.retweet()
+                    print('Retweeted the tweet')
+
+            sleep(SLEEP_TIME)
+
+        except tweepy.errors.TweepyException as e:
+            print(e)
+
+        except StopIteration:
+            break
+
+
+
     
 
 
---
+++ config.py
@@ -0,0 +1,5 @@
+QUERY = '#NBA2K23'
+LIKE = True
+RETWEET = True
+FOLLOW = True
+SLEEP_TIME = 60
